5e5954c72138c1af289be611fdd3c06d101fd323
Milen Stanev <milen.stanev@gmail.com>
2017-05-28 18:49:31 +0300

    Added TODO's

--- src/app.home.js
+++ src/app.home.js
@@ -5,7 +5,11 @@ import HomeCtrl from './app.homeCtrl.js';
 const appHome = angular.module('app.home', []);
 
 appHome.component('app.home', {
-  template: '<h1>Home</h1><p>Hello, {{ $ctrl }}!</p>',
+  template: '' +
+              '<h1>Home</h1>' +
+              '<p>Hello, {{ $ctrl }}!</p>' +
+              'data: {{ $ctrl.map.get("data") }}' +
+              '<div ng-repeat="items in $ctrl.getMapData()">{{items.title}}</div>',
   controller: HomeCtrl
 });
 
--- src/app.homeCtrl.js
+++ src/app.homeCtrl.js
@@ -13,30 +13,43 @@ function enumerable(value) {
     descriptor.enumerable = value;
     descriptor.configurable = true;
 
-    debugger // TODO: See this :) Object.define .etc
+    //debugger // TODO: See this :) Object.define .etc
     return descriptor;
   }
 }
 
 console.log(new C());
 
-
+//todo: $inject decorator
 class HomeCtrl {
-  constructor($http) {
-    this.user = {
-      name: 'User name'
-    };
-
-    $http.get('http://localhost:3000/api//svc/translations').then(
-      res => console.log(res)
-    )
-  }
-
+  user = {
+    name: 'User name'
+  };
+  map = new Map();
   static asd = "Asd";
-
   qwe = "qwe";
 
-  init() {}
+  init() {
+    this.map.set('data', [
+      {title: '1'},
+      {title: '2'},
+      {title: '3'}
+    ]);
+  }
+
+  getMapData() {
+    const data = this.map.get('data');
+
+    return data;
+  }
+
+  constructor($http) {
+    $http.get('http://localhost:3000/api//svc/translations').then(
+      res => console.log(res)
+    );
+
+    this.init();
+  }
 }
 
 HomeCtrl.$inject = ['$http'];
